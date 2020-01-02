e5e391956232870da9c4f21f80eb9a693908188e
vuongduong12345 <vuongtatduong@gmail.com>
2020-01-02 11:25:31 +0700

    Create Result.vue

---
+++ src/components/Result.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>This is Results {{ result }} </p>
+</template>
+<script>
+export default {
+    // computed chạy trước khi data đc load, hthi dc result lấy từ store
+    computed: {
+        result() {
+            return this.$store.state.result
+        }
+    }
+}
+</script>
+<style>
+
+</style>
