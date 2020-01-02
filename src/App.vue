69aa66120f8e85fa7e86d84e80abe2c6a3728e37
vuongduong12345 <vuongtatduong@gmail.com>
2020-01-02 11:25:41 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Vuex</h1>
+    <app-result/>
+    <app-counter/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import AppResult from "./components/Result";
+import AppCounter from "./components/Counter";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    AppResult,
+    AppCounter
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
