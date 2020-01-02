8f2a4020a836dcc9ac87fa17e37b2a3fabf6dcb0
vuongduong12345 <vuongtatduong@gmail.com>
2020-01-02 11:25:17 +0700

    Create Counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button class="btn btn-succes" @click="increment">Increment</button>
+    <button class="btn btn-succes" @click="decrement">Decrement</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    increment() {
+      this.$store.state.result++;
+    },
+    decrement() {
+      this.$store.state.result--;
+    }
+  }
+};
+</script>
+<style></style>
